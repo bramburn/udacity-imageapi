4a400a6ce938047d5d3fe71cfbc4ab8d06108fb6
Bhavesh Ramburn <nitrogen@gmail.com>
2021-12-11 19:59:48 +0000

    notes

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    ignorePatterns: ["node_modules","build"],
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+
+    ],
+    "rules": {
+    },
+};
