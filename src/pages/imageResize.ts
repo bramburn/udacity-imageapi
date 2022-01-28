40c3888944d00ba6d290a0b36dd2943a492a29af
Bhavesh Ramburn <nitrogen@gmail.com>
2022-01-28 10:56:27 +0000

    fix: prettier and updated the sharp and jasmine package

--- src/pages/imageResize.ts
+++ src/pages/imageResize.ts
@@ -1,4 +1,4 @@
-import { Router, Request,Response } from 'express'
+import { Router, Request, Response } from 'express'
 import paramsChecker from '../middleware/paramsChecker'
 import { fileCheckExists } from '../classes/fileCheckClass'
 import Path from 'path'
@@ -7,40 +7,43 @@ import { ImageResizeClass } from '../classes/imageResizeClass'
 const imageResizeRouter = Router()
 imageResizeRouter.use(paramsChecker)
 
-imageResizeRouter.get('/', async (req:Request, res:Response): Promise<void> => {
-    const imageFileName = <string>req.query.image
-    const width = parseInt(<string>req.query.width)
-    const height = parseInt(<string>req.query.height)
-    //check if the file exists
-    const sourcePath = Path.join(__dirname, '../images', imageFileName)
+imageResizeRouter.get(
+    '/',
+    async (req: Request, res: Response): Promise<void> => {
+        const imageFileName = <string>req.query.image
+        const width = parseInt(<string>req.query.width)
+        const height = parseInt(<string>req.query.height)
+        //check if the file exists
+        const sourcePath = Path.join(__dirname, '../images', imageFileName)
 
-    if (!(await fileCheckExists(sourcePath))) {
-        console.error('Image does not exists')
-        res.status(500).end()
+        if (!(await fileCheckExists(sourcePath))) {
+            console.error('Image does not exists')
+            res.status(500).end()
+            return
+        }
+
+        // resize and serve
+        const cachedImage = Path.join(
+            __dirname,
+            '../cached',
+            `${width}_${height}_${imageFileName}`
+        )
+
+        if (!(await fileCheckExists(cachedImage))) {
+            //create the file
+            try {
+                const im = new ImageResizeClass()
+                await im.resizeImage(sourcePath, cachedImage, width, height)
+            } catch {
+                console.error('Error building the image')
+                res.status(500).end()
+            }
+        }
+        // else serve it
+
+        res.sendFile(cachedImage)
         return
     }
-
-    // resize and serve
-    const cachedImage = Path.join(
-        __dirname,
-        '../cached',
-        `${width}_${height}_${imageFileName}`
-    )
-
-    if (!(await fileCheckExists(cachedImage))) {
-        //create the file
-        try {
-            const im = new ImageResizeClass()
-            await im.resizeImage(sourcePath, cachedImage, width, height)
-        } catch {
-            console.error('Error building the image')
-            res.status(500).end()
-        }
-    }
-    // else serve it
-
-    res.sendFile(cachedImage)
-    return
-})
+)
 
 export default imageResizeRouter
